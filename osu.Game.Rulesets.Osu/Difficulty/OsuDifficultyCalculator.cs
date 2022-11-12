5b65bb6ecaf528d6eca6737d208e33e546566c92
tsunyoku <mbruhyo@gmail.com>
2022-11-12 13:40:14 +0000

    Nerf aim and flashlight by attraction strength

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -57,6 +57,16 @@ namespace osu.Game.Rulesets.Osu.Difficulty
                 flashlightRating *= 0.7;
             }
 
+            var magnetisedMod = mods.FirstOrDefault(h => h is OsuModMagnetised);
+
+            if (magnetisedMod is OsuModMagnetised mod)
+            {
+                double magnetisedNerfFactor = 1 - Math.Pow(mod.AttractionStrength.Value, 2);
+
+                aimRating *= magnetisedNerfFactor;
+                flashlightRating *= magnetisedNerfFactor * 0.5;
+            }
+
             double baseAimPerformance = Math.Pow(5 * Math.Max(1, aimRating / 0.0675) - 4, 3) / 100000;
             double baseSpeedPerformance = Math.Pow(5 * Math.Max(1, speedRating / 0.0675) - 4, 3) / 100000;
             double baseFlashlightPerformance = 0.0;
