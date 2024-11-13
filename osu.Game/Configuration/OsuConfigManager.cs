383c984d50b1b15ec4921cee95a847de2b439cb9
tsunyoku <tsunyoku@gmail.com>
2024-11-13 16:13:47 +0000

    Rebase fixes

--- osu.Game/Configuration/OsuConfigManager.cs
+++ osu.Game/Configuration/OsuConfigManager.cs
@@ -214,6 +214,8 @@ namespace osu.Game.Configuration
             SetDefault(OsuSetting.EditorContractSidebars, false);
 
             SetDefault(OsuSetting.AlwaysShowHoldForMenuButton, false);
+
+            SetDefault(OsuSetting.BroadcastGameState, false);
         }
 
         protected override bool CheckLookupContainsPrivateInformation(OsuSetting lookup)
@@ -444,5 +446,6 @@ namespace osu.Game.Configuration
         EditorRotationOrigin,
         EditorTimelineShowBreaks,
         EditorAdjustExistingObjectsOnTimingChanges,
+        BroadcastGameState,
     }
 }
