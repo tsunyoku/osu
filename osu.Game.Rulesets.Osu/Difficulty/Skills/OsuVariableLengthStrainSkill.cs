42e41630acb8b6fae0260ed1d12201d7f5b7c9e3
James Wilson <tsunyoku@gmail.com>
2025-09-28 18:01:19 +0100

    Remove all strain reduction

--- osu.Game.Rulesets.Osu/Difficulty/Skills/OsuVariableLengthStrainSkill.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/OsuVariableLengthStrainSkill.cs
@@ -2,9 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using osu.Framework.Utils;
 using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Mods;
 
@@ -12,17 +10,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 {
     public abstract class OsuVariableLengthStrainSkill : VariableLengthStrainSkill
     {
-        /// <summary>
-        /// The duration strain reduction will apply to.
-        /// We assume that the first seconds of the map are always easier than calculated difficulty due to them being free to retry.
-        /// </summary>
-        protected virtual int ReducedDuration => 40;
-
-        /// <summary>
-        /// The baseline multiplier applied to the section with the biggest strain.
-        /// </summary>
-        protected virtual double ReducedStrainBaseline => 0.727;
-
         protected override double DifficultyMultiplier => 1.058;
 
         protected OsuVariableLengthStrainSkill(Mod[] mods)
@@ -38,68 +25,11 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             // These sections will not contribute to the difficulty.
             var peaks = GetCurrentStrainPeaks().Where(p => p.Value > 0);
 
-            List<StrainPeak> strains = peaks.OrderByDescending(p => p.Value).ToList();
-
-            double time = 0; // Time is measured in units of strains
-            const int chunk_size = 20;
-            int strainsToRemove = 0;
-
-            double reducedSectionCount = ReducedDuration * 1000.0 / MaxSectionLength;
-
-            // We are reducing the highest strains first to account for extreme difficulty spikes
-            // Split the strain into 20ms chunks to try to mitigate inconsistencies caused by reducing strains
-            while (strains.Count > strainsToRemove && time < ReducedDuration)
-            {
-                StrainPeak strain = strains[strainsToRemove];
-                double addedTime = 0;
-
-                while (addedTime < strain.SectionLength)
-                {
-                    double scale = Math.Log10(Interpolation.Lerp(1, 10, Math.Clamp((time + addedTime) / MaxSectionLength / reducedSectionCount, 0, 1)));
-
-                    strains.Add(new StrainPeak(strain.Value * Interpolation.Lerp(ReducedStrainBaseline, 1.0, scale), Math.Min(chunk_size, strain.SectionLength - addedTime)));
-                    addedTime += chunk_size;
-                }
-
-                time += strain.SectionLength;
-                strainsToRemove++;
-            }
-
-            strains.RemoveRange(0, strainsToRemove);
-
             // Reset time for summing
-            time = 0;
-
-            // Difficulty is a continuous weighted sum of the sorted strains
-            foreach (StrainPeak strain in strains.OrderByDescending(s => s.Value))
-            {
-                /* Weighting function:
-                        a+b
-                        ∫ 0.9^x dx
-                        a
-                    where a = startTime and b = strain.SectionLength */
-                double weight = Math.Pow(DecayWeight, time) * (DecayWeightIntegral - DecayWeightIntegral * Math.Pow(DecayWeight, strain.SectionLength / MaxSectionLength));
-                difficulty += strain.Value * weight;
-                time += strain.SectionLength / MaxSectionLength;
-            }
-
-            return difficulty * DifficultyMultiplier;
-        }
-
-        private double calculateNonReductionDifficultyValue()
-        {
-            double difficulty = 0;
-
-            // Sections with 0 strain are excluded to avoid worst-case time complexity of the following sort (e.g. /b/2351871).
-            // These sections will not contribute to the difficulty.
-            var peaks = GetCurrentStrainPeaks().Where(p => p.Value > 0);
-
-            List<StrainPeak> strains = peaks.OrderByDescending(p => p.Value).ToList();
-
             double time = 0;
 
             // Difficulty is a continuous weighted sum of the sorted strains
-            foreach (StrainPeak strain in strains.OrderByDescending(s => s.Value))
+            foreach (StrainPeak strain in peaks.OrderByDescending(s => s.Value))
             {
                 /* Weighting function:
                         a+b
@@ -119,7 +49,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         /// </summary>
         public double CountRelevantObjects()
         {
-            double consistentTopStrain = calculateNonReductionDifficultyValue() / 10; // What would the top strain be if all strain values were identical
+            double consistentTopStrain = DifficultyValue() / 10; // What would the top strain be if all strain values were identical
 
             if (consistentTopStrain == 0)
                 return 0.0;
