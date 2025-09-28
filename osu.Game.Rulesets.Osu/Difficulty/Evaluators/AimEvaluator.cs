42f0a9453f056e01bfe9a3381859890a9c995bb5
James Wilson <tsunyoku@gmail.com>
2025-09-28 15:15:24 +0100

    Apply delta corrections

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -35,6 +35,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             var osuLastObj = (OsuDifficultyHitObject)current.Previous(0);
             var osuLastLastObj = (OsuDifficultyHitObject)current.Previous(1);
             var osuLast2Obj = (OsuDifficultyHitObject)current.Previous(2);
+            var osuLast3Obj = (OsuDifficultyHitObject)current.Previous(3);
 
             const int radius = OsuDifficultyHitObject.NORMALISED_RADIUS;
             const int diameter = OsuDifficultyHitObject.NORMALISED_DIAMETER;
@@ -149,6 +150,17 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 sliderBonus = osuLastObj.TravelDistance / osuLastObj.TravelTime;
             }
 
+            if (osuLast3Obj != null)
+            {
+                aimStrain *= calculateJumpOverlapCorrection((OsuHitObject)osuCurrObj.BaseObject,
+                    (OsuHitObject)osuLastObj.BaseObject,
+                    (OsuHitObject)osuLastLastObj.BaseObject,
+                    (OsuHitObject)osuLast3Obj.BaseObject);
+            }
+
+            aimStrain *= calculateSmallJumpNerf(osuCurrObj);
+            aimStrain *= calculateBigJumpBuff(osuCurrObj);
+
             aimStrain += wiggleBonus * wiggle_multiplier;
             aimStrain += velocityChangeBonus * velocity_change_multiplier;
 
@@ -168,5 +180,61 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         private static double calcWideAngleBonus(double angle) => DifficultyCalculationUtils.Smoothstep(angle, double.DegreesToRadians(40), double.DegreesToRadians(140));
 
         private static double calcAcuteAngleBonus(double angle) => DifficultyCalculationUtils.Smoothstep(angle, double.DegreesToRadians(140), double.DegreesToRadians(40));
+
+        /// <summary>
+        /// We apply nerf to jumps within ~1-3.5 distance (with peak at 2.2) depending on BPM.
+        /// Graphs: https://www.desmos.com/calculator/lbwtkv1qom
+        /// </summary>
+        private static double calculateSmallJumpNerf(OsuDifficultyHitObject curr)
+        {
+            var effectiveBPM = 30 / (curr.AdjustedDeltaTime / 1000.0);
+
+            // this applies nerf up to 300 bpm and starts deminishing it at ~200 bpm
+            var bpmCutoff = DifficultyCalculationUtils.Logistic(-((255 - effectiveBPM) / 10.0));
+
+            var distanceNerf = Math.Exp(-Math.Pow(((curr.LazyJumpDistance / OsuDifficultyHitObject.NORMALISED_DIAMETER) - 2.2) / 0.7, 2.0));
+
+            return 1.0 - distanceNerf * bpmCutoff * 0.25;
+        }
+
+        /// <summary>
+        /// We apply buff to jumps with distance starting from ~4 on low BPMs.
+        /// Graphs: https://www.desmos.com/calculator/fmewz0foql
+        /// </summary>
+        private static double calculateBigJumpBuff(OsuDifficultyHitObject curr)
+        {
+            var effectiveBPM = 30 / (curr.AdjustedDeltaTime / 1000.0);
+
+            // this applies buff up until ~250 bpm
+            var bpmCutoff = DifficultyCalculationUtils.Logistic(-((210 - effectiveBPM) / 8.0));
+
+            var distanceBuff = DifficultyCalculationUtils.Logistic(-(((curr.LazyJumpDistance / OsuDifficultyHitObject.NORMALISED_DIAMETER) - 6.0) / 0.5));
+
+            return 1.0 + distanceBuff * bpmCutoff * 0.5;
+        }
+
+        /// <summary>
+        /// We apply a nerf to big jumps where second-last or fourth-last and current objects are close.
+        /// This mainly targets repeating jumps such as
+        /// 1  3
+        ///  \/
+        ///  /\
+        /// 4  2
+        /// </summary>
+        private static double calculateJumpOverlapCorrection(OsuHitObject curr, OsuHitObject last, OsuHitObject lastLast, OsuHitObject last3)
+        {
+            float scalingFactor = OsuDifficultyHitObject.NORMALISED_RADIUS / (float)curr.Radius;
+
+            var lastLastToCurrDist = (curr.StackedPosition * scalingFactor - lastLast.StackedPosition * scalingFactor).Length / OsuDifficultyHitObject.NORMALISED_DIAMETER;
+            var last3ToCurrDist = (curr.StackedPosition * scalingFactor - last3.StackedPosition * scalingFactor).Length / OsuDifficultyHitObject.NORMALISED_DIAMETER;
+            var lastToCurrDist = (curr.StackedPosition * scalingFactor - last.StackedPosition * scalingFactor).Length / OsuDifficultyHitObject.NORMALISED_DIAMETER;
+
+            var secondLastToCurrentNerf = Math.Max(0.15 - 0.1 * lastLastToCurrDist, 0.0);
+            var fourthLastToCurrentNerf = Math.Max(0.1125 - 0.075 * last3ToCurrDist, 0.0);
+
+            var distanceCutoff = DifficultyCalculationUtils.Logistic(-((lastToCurrDist - 3.3) / 0.25));
+
+            return 1.0 - (secondLastToCurrentNerf + fourthLastToCurrentNerf) * distanceCutoff;
+        }
     }
 }
